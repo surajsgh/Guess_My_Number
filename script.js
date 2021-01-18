affd57cac1b52de32b8efe4e1d6f74f07f48ce80
Suraj Singh <surajsingh220698@outlook.com>
2021-01-19 00:30:06 +0530

    Add the DOM manipulation introduction.

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 'use strict';
+
+// This line of code is gonna give you the whole html content stored inside the html element with the 'message' class name.
+console.log(document.querySelector('.message'));
+
+// This line of code is just gonna give you the text content.
+console.log(document.querySelector('.message').textContent);
