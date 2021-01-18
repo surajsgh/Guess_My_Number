c305601c666b015047e3487dd149b2983e2d99d9
Suraj Singh <surajsingh220698@outlook.com>
2021-01-19 00:52:21 +0530

    Add the usage of value attribute in DOM manipulations for input fields.

--- script.js
+++ script.js
@@ -5,3 +5,12 @@ console.log(document.querySelector('.message'));
 
 // This line of code is just gonna give you the text content.
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'Correct Number!';
+
+document.querySelector('.number').textContent = 20;
+
+document.querySelector('.score').textContent = 10;
+
+// For input values we rather use value attribute(No usage of textContent attribute.)
+document.querySelector('.guess').value = 15;
