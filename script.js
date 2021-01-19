69bea31a2d22fc3b2db6f3dff61957dd8fba75d9
Suraj Singh <surajsingh220698@outlook.com>
2021-01-20 01:00:24 +0530

    Add the reset functionality on clicking the again button.

--- script.js
+++ script.js
@@ -20,7 +20,6 @@ document.querySelector('.guess').value = 15;
 // Random number generation
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -33,6 +32,8 @@ document.querySelector('.check').addEventListener('click', function () {
   // When player wins!
   else if (secretNumber === guess) {
     document.querySelector('.message').textContent = 'Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
 
@@ -62,4 +63,18 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.score').textContent = 0;
     }
   }
+});
+
+document.querySelector('.again').addEventListener('click', function () {
+  document.querySelector('.number').textContent = '?';
+
+  document.querySelector('.message').textContent = 'Start guessing...';
+
+  document.querySelector('.score').textContent = '20';
+
+  document.querySelector('.guess').value = '';
+
+  document.querySelector('body').style.backgroundColor = '#222';
+
+  document.querySelector('.number').style.width = '15rem';
 });
